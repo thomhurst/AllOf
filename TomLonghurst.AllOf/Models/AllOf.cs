ef7a1f7e6a76bd9f18ff4881cbcc3624cb98f53c
Tom Longhurst <tom.longhurst@asos.com>
2022-09-26 09:44:43 +0100

    Virtual method

--- TomLonghurst.AllOf/Models/AllOf.cs
+++ TomLonghurst.AllOf/Models/AllOf.cs
@@ -3,7 +3,7 @@
 public class AllOf<T> : IAllOf<T>
 {
     private readonly IAllOfImplementationWrapper<T> _allOfImplementationWrapper;
-    public IEnumerable<T> Items { get; }
+    public virtual IEnumerable<T> Items { get; }
 
     public AllOf(IEnumerable<T> items, IAllOfImplementationWrapper<T> allOfImplementationWrapper)
     {
@@ -11,7 +11,7 @@ public class AllOf<T> : IAllOf<T>
         Items = items;
     }
 
-    public T OnEach() => _allOfImplementationWrapper.Value;
+    public virtual T OnEach() => _allOfImplementationWrapper.Value;
 
     public static implicit operator T(AllOf<T> allOf) => allOf.OnEach();
 }
