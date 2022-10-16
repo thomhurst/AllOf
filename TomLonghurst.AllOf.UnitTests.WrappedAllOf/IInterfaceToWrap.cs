41b4a91b089da1d2c17b742be6e5b4295bb30a3a
Tom Longhurst <thomhurst@gmail.com>
2022-10-16 19:11:41 +0100

    Wrapper tests

---
+++ TomLonghurst.AllOf.UnitTests.WrappedAllOf/IInterfaceToWrap.cs
@@ -0,0 +1,30 @@
+﻿namespace TomLonghurst.AllOf.UnitTests.WrappedAllOf;
+
+public interface IInterfaceToWrap
+{
+    void Blah();
+}
+
+public class Impl1 : IInterfaceToWrap
+{
+    public void Blah()
+    {
+        Console.WriteLine(GetType().Name);
+    }
+}
+
+public class Impl2 : IInterfaceToWrap
+{
+    public void Blah()
+    {
+        Console.WriteLine(GetType().Name);
+    }
+}
+
+public class Impl3 : IInterfaceToWrap
+{
+    public void Blah()
+    {
+        Console.WriteLine(GetType().Name);
+    }
+}
---
+++ TomLonghurst.AllOf.UnitTests.WrappedAllOf/Tests.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using TomLonghurst.AllOf.Extensions;
+using TomLonghurst.AllOf.Models;
+
+namespace TomLonghurst.AllOf.UnitTests.WrappedAllOf;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        var serviceProvider = new ServiceCollection()
+            .AddSingleton<IInterfaceToWrap, Impl1>()
+            .AddSingleton<IInterfaceToWrap, Impl2>()
+            .AddSingleton<IInterfaceToWrap, Impl3>()
+            .AddTransient(typeof(PublisherOf<>), typeof(PublisherOfImpl<>))
+            .AddAllOfs()
+            .BuildServiceProvider();
+        
+        var myInterface = serviceProvider.GetRequiredService<PublisherOf<IInterfaceToWrap>>();
+        
+        Assert.That(myInterface.GetType().Name, Is.EqualTo("PublisherOfImpl`1"));
+        Assert.That(myInterface.ForEachSubscriber(), Is.Not.Null);
+    }
+}
