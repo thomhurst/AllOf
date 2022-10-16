04bf42599e81f7cbef482164f18d63fa74fb365f
Tom Longhurst <thomhurst@gmail.com>
2022-10-16 20:59:21 +0100

    Split out test classes

--- TomLonghurst.AllOf.UnitTests.WrappedAllOf/IInterfaceToWrap.cs
+++ TomLonghurst.AllOf.UnitTests.WrappedAllOf/IInterfaceToWrap.cs
@@ -4,27 +4,3 @@ public interface IInterfaceToWrap
 {
     void Blah();
 }
-
-public class Impl1 : IInterfaceToWrap
-{
-    public void Blah()
-    {
-        Console.WriteLine(GetType().Name);
-    }
-}
-
-public class Impl2 : IInterfaceToWrap
-{
-    public void Blah()
-    {
-        Console.WriteLine(GetType().Name);
-    }
-}
-
-public class Impl3 : IInterfaceToWrap
-{
-    public void Blah()
-    {
-        Console.WriteLine(GetType().Name);
-    }
-}
---
+++ TomLonghurst.AllOf.UnitTests.WrappedAllOf/Impl1.cs
@@ -0,0 +1,9 @@
+﻿namespace TomLonghurst.AllOf.UnitTests.WrappedAllOf;
+
+public class Impl1 : IInterfaceToWrap
+{
+    public void Blah()
+    {
+        Console.WriteLine(GetType().Name);
+    }
+}
